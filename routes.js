8e1c9a051267b934e50cffa024e77c340a5e3c9f
Lee Morgan <me@leemorgan.io>
2022-08-24 15:24:28 +0300

    Remove "/api" from routes.

--- routes.js
+++ routes.js
@@ -2,13 +2,13 @@ const vendor = require("./controllers/vendor.js");
 
 module.exports = (app)=>{
     //DOCS
-    app.get("/api", (req, res)=>{res.sendFile(`${__dirname}/api.html`)});
-    app.get("/api.css", (req, res)=>{res.sendFile(`${__dirname}/api.css`)});
+    app.get("/docs", (req, res)=>{res.sendFile(`${__dirname}/api.html`)});
+    app.get("/docs.css", (req, res)=>{res.sendFile(`${__dirname}/api.css`)});
 
     //VENDOR
-    app.post("/api/vendor", vendor.create);
-    app.put("/api/vendor", vendor.update);
-    app.post("/api/vendor/login", vendor.login);
-    app.get("/api/vendor/logout", vendor.logout);
-    app.get("/api/vendor/:id", vendor.retrieve);
+    app.post("/vendor", vendor.create);
+    app.put("/vendor", vendor.update);
+    app.post("/vendor/login", vendor.login);
+    app.get("/vendor/logout", vendor.logout);
+    app.get("/vendor/:id", vendor.retrieve);
 }
