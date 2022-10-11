bcd3ce28350358c02d30f4d7cbbaac523a7e4644
Lee Morgan <me@leemorgan.io>
2022-10-11 19:55:28 -0400

    Add route for creating new products for a vendor.

--- controllers/product.js
+++ controllers/product.js
@@ -4,13 +4,33 @@ module.exports = {
     /*
     POST: create a new product for a vendor
     req.body = {
-        token: JSON web token, required
-        name: String, required
-        unit: String, optional
-        quantity: Number, optional
+        products: [{
+            name: String, required
+            unit: String, optional
+            quantity: Number, optional
+        }]
     }
     */
     create: function(req, res){
-        console.log("creating");
+        let newProducts = [];
+        for(let i = 0; i < req.body.products.length; i++){
+            let product = new Product({
+                name: req.body.products[i].name,
+                unit: req.body.products[i].unit,
+                quantity: req.body.products[i].quantity
+            });
+
+            res.locals.vendor.products.push(product);
+            newProducts.push(product);
+        }
+
+        res.locals.vendor.save()
+            .then((vendor)=>{
+                return res.json(newProducts);
+            })
+            .catch((err)=>{
+                console.error(err);
+                return res.json("ERROR: unable to create new products");
+            });
     }
 }
